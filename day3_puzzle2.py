fbe0d0f1dd2599c6da7acd099236faf38d249dc3
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-03 01:02:18 -0500

    Commented code and unused function

--- day3_puzzle2.py
+++ day3_puzzle2.py
@@ -4,21 +4,6 @@ from sys import argv
 from data_reading import data_from_lines
 
 
-#def _find_badge_type(rucksacks):
-#	for rucksack in rucksacks:
-
-
-def _common_letter_in_strings(str1, str2, ignored):
-	for letter1 in str1:
-
-		for letter2 in str2:
-
-			if letter1 == letter2 and letter1 not in ignored:
-				return letter1
-
-	return None
-
-
 data_path = Path(argv[1])
 
 rucksacks = data_from_lines(data_path)
