4cf29822fb288d3a1456bda318294eec8d27ce80
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-04 00:16:50 -0500

    Day 4 puzzle 1 answer: 560

---
+++ day4_puzzle1.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from sys import argv
+
+from data_reading import data_from_lines
+
+
+class _Range:
+
+	def __init__(self, lower, upper):
+		self._lower = lower
+		self._upper = upper
+
+	def includes(self, other):
+		return self._lower <= other._lower and self._upper >= other._upper
+
+
+def _get_ranges_from_pair(pair):
+	ranges = pair.split(",")
+	range1 = ranges[0].split("-")
+	range2 = ranges[1].split("-")
+	return\
+			_Range(int(range1[0]), int(range1[1])),\
+			_Range(int(range2[0]), int(range2[1]))
+
+
+data_path = Path(argv[1])
+
+pairs = data_from_lines(data_path, _get_ranges_from_pair)
+
+overlaps = 0
+for pair in pairs:
+	range1 = pair[0]
+	range2 = pair[1]
+
+	if range1.includes(range2) or range2.includes(range1):
+		overlaps += 1
+
+print(overlaps)
