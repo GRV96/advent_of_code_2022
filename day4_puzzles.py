d7d60b23448e1d5be3193d37f784ec9ca26b7d5a
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-04 00:50:05 -0500

    Better method names

--- day4_puzzles.py
+++ day4_puzzles.py
@@ -13,14 +13,14 @@ class _Range:
 	def __repr__(self):
 		return self.__class__.__name__ + f"({self._lower}, {self._upper})"
 
-	def contains(self, number):
+	def contains_num(self, number):
 		return self._lower <= number and number <= self._upper
 
-	def includes(self, other):
+	def contains_range(self, other):
 		return self._lower <= other._lower and self._upper >= other._upper
 
 	def overlaps(self, other):
-		return self.contains(other._lower) or self.contains(other._upper)
+		return self.contains_num(other._lower) or self.contains_num(other._upper)
 
 
 def _get_ranges_from_pair(pair):
@@ -36,7 +36,7 @@ data_path = Path(argv[1])
 
 puzzle_num = int(argv[2])
 if puzzle_num == 1:
-	detection = lambda r1, r2: r1.includes(r2) or r2.includes(r1)
+	detection = lambda r1, r2: r1.contains_range(r2) or r2.contains_range(r1)
 elif puzzle_num == 2:
 	detection = lambda r1, r2: r1.overlaps(r2) or r2.overlaps(r1)
 else:
