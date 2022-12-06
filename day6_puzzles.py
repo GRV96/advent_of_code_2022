ef9e99395b8e4942024a24130f29d431d4b950e7
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-06 00:55:07 -0500

    d6 p2 wrong answer: 8

--- day6_puzzles.py
+++ day6_puzzles.py
@@ -20,7 +20,7 @@ def _find_first_repeated_char(signal, w_start, w_length):
 	return repeated_char_index
 
 
-def _find_marker_end(signal, start, window_length):
+def _find_marker_index(signal, start, window_length, find_end):
 	index = start
 	while True:
 		index_found = _find_first_repeated_char(signal, index, window_length)
@@ -31,15 +31,21 @@ def _find_marker_end(signal, start, window_length):
 		else:
 			index = index_found + 1
 
-	return index + window_length
+	if find_end:
+		marker_index = index + window_length
+
+	else:
+		marker_index = start
+
+	return marker_index
 
 
 data_path = Path(argv[1])
 
 signal = lines_from_file(data_path)[0]
 
-packet_m_e = _find_marker_end(signal, 0, 4)
-message_m_e = _find_marker_end(signal, packet_m_e+1, 14)
+packet_m_e = _find_marker_index(signal, 0, 4, True)
+message_m_e = _find_marker_index(signal, packet_m_e+1, 14, False)
 
 print(f"Packet marker end: {packet_m_e}")
 print(f"Message marker end: {message_m_e}")
